35c1c252b22616272dfcf9bc6194f0a4321b9388
jillfarns <jillfarnsworth@yahoo.com>
2020-02-21 14:30:48 -0800

    moved helpers

--- app.rb
+++ app.rb
@@ -28,58 +28,6 @@ $success_url = 'http://localhost:4567/success?session_id={CHECKOUT_SESSION_ID}'
 $cancel_url = 'http://localhost:4567/cart'
 
 
-# returns JSON product data from Products API resource
-def parse_products
-	response = JSON.parse(Stripe::Product.list().to_s)
-	response["data"]
-end
-
-# converts array of JSON product data into hash of products where product ID is the key and Album object is the value
-def create_album_hash(products_array)
-	album_hash = {}
-	products_array.each do |product| 
-		album = Album.new(product["name"], product["metadata"]["price"], product["images"][0])
-		album_hash[product["id"]] = album
-	end
-	album_hash
-end
-
-# creates session to pass id to redirectToCheckout for Checkout functionality
-def create_stripe_session
-	items=[]
-	$cart.each do |product_id, quantity|
-  		item = {}
-  		item["amount"] = $products[product_id].price.to_i * 100
-  		item["currency"] = 'usd'
-  		item["name"] = $products[product_id].name
-  		item["quantity"] = quantity
-  		items.push(item)
-  	end
-	session = Stripe::Checkout::Session.create(
-  	payment_method_types: ['card'],
-  	line_items: items,
-  	success_url: $success_url,
-  	cancel_url: $cancel_url
-)
-end	
-
-#function to get 5 most recent charges and look to match to session on payment intent. NOTE: see README to see how I would use webhooks instead
-def get_charge_id  
-  charge_id = nil
-  if $session_id != nil
-    session = Stripe::Checkout::Session.retrieve($session_id)
-    charge_list = Stripe::Charge.list({limit: 5})
-    charge_list["data"].each do |charge|
-      if charge["payment_intent"] == session["payment_intent"] && charge["status"] == "succeeded"
-        charge_id = charge["id"]
-        break
-      end
-    end
-  end
-  puts "charge_id not found" if charge_id == nil
-  charge_id
-end
-
 get '/' do 
 	$products = create_album_hash(parse_products)
 	erb :index, :locals => {:products => $products}
@@ -123,3 +71,60 @@ end
 post '/goToCheckout' do
     redirect '/checkout'
 end
+
+
+
+helpers do
+  # returns JSON product data from Products API resource
+  def parse_products
+    response = JSON.parse(Stripe::Product.list().to_s)
+    response["data"]
+  end
+
+  # converts array of JSON product data into hash of products where product ID is the key and Album object is the value
+  def create_album_hash(products_array)
+    album_hash = {}
+    products_array.each do |product| 
+      album = Album.new(product["name"], product["metadata"]["price"], product["images"][0])
+      album_hash[product["id"]] = album
+    end
+    album_hash
+  end
+
+  # creates session to pass id to redirectToCheckout for Checkout functionality
+  def create_stripe_session
+    items=[]
+    $cart.each do |product_id, quantity|
+        item = {}
+        item["amount"] = $products[product_id].price.to_i * 100
+        item["currency"] = 'usd'
+        item["name"] = $products[product_id].name
+        item["quantity"] = quantity
+        items.push(item)
+      end
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: items,
+      success_url: $success_url,
+      cancel_url: $cancel_url
+  )
+  end 
+
+  #function to get 5 most recent charges and look to match to session on payment intent. NOTE: see README to see how I would use webhooks instead
+  def get_charge_id  
+    charge_id = nil
+    if $session_id != nil
+      session = Stripe::Checkout::Session.retrieve($session_id)
+      charge_list = Stripe::Charge.list({limit: 5})
+      charge_list["data"].each do |charge|
+        if charge["payment_intent"] == session["payment_intent"] && charge["status"] == "succeeded"
+          charge_id = charge["id"]
+          break
+        end
+      end
+    end
+    puts "charge_id not found" if charge_id == nil
+    charge_id
+  end
+end
+
