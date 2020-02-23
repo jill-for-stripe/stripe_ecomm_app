24f3003287c969e355fca1d65a8d3b26d78af69a
jillfarns <jillfarnsworth@yahoo.com>
2020-02-23 11:45:59 -0800

    pull out helper methods into helpers.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'money'
 
 require_relative 'models/album'
+require_relative 'helpers.rb'
 
 # global hash representation of the available products, where the product ID is the key and an Album is the value
 $products = Hash.new
@@ -71,56 +72,6 @@ post '/goToCheckout' do
     redirect '/checkout'
 end
 
-
-
 helpers do
-  # returns JSON product data from Products API resource
-  def parse_products
-    response = JSON.parse(Stripe::Product.list().to_s)
-    response["data"]
-  end
-
-  # converts array of JSON product data into hash of products where product ID is the key and Album object is the value
-  def create_album_hash(products_array)
-    album_hash = {}
-    products_array.each do |product| 
-      album = Album.new(product["name"], product["metadata"]["price"], product["images"][0])
-      album_hash[product["id"]] = album
-    end
-    album_hash
-  end
-
-  # creates session to pass id to redirectToCheckout for Checkout functionality
-  def create_stripe_session
-    items=[]
-    $cart.each do |product_id, quantity|
-        item = {}
-        item["amount"] = $products[product_id].price.to_i * 100
-        item["currency"] = 'usd'
-        item["name"] = $products[product_id].name
-        item["quantity"] = quantity
-        items.push(item)
-      end
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: items,
-      success_url: $success_url,
-      cancel_url: $cancel_url
-  )
-  end 
-
-  #function to get 5 most recent charges and look to match to session on payment intent. NOTE: see README to see how I would use webhooks instead
-  def get_charge_id  
-    if $session_id != nil
-      session = Stripe::Checkout::Session.retrieve($session_id)
-      charge_list = Stripe::Charge.list({limit: 5})
-      charge_list["data"].each do |charge|
-        if charge["payment_intent"] == session["payment_intent"] && charge["status"] == "succeeded"
-          return charge["id"]
-        end
-      end
-    end
-  end
-
+  include Helpers
 end
-
