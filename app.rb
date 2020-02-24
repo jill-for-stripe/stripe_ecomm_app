1872923151b12bf1fabfe0213750ec4fdabc9091
jillfarns <jillfarnsworth@yahoo.com>
2020-02-23 20:08:11 -0800

    created Application class; changed products from global to instance variable; changed cart, subtotal, session_id from global to class variables

--- app.rb
+++ app.rb
@@ -7,71 +7,70 @@ require 'money'
 require_relative 'models/album'
 require_relative 'helpers.rb'
 
-# global hash representation of the available products, where the product ID is the key and an Album is the value
-$products = Hash.new
+class Application < Sinatra::Base
+  
+  def initialize
+    @products = create_album_hash(parse_products)
+    @@cart = Hash.new(0)
+    @@subtotal = Money.new(0, "USD")
+    @@session_id = nil
+    super
+  end
 
-# global hash representation of the cart, where the product ID is the key and the quantity is the value
-$cart = Hash.new(0)
+  # set Stripe API key
+  Stripe.api_key = 'sk_test_o20UzauzBU7uIE2G7CoaMA3q00T3v4QoUU'
 
-# global variable to store the charge object once succeeded 
-$charge = nil
-
-# global variable to store the session id once the session is created
-$session_id = nil
-
-# subtotal of items in the cart
-$subtotal = Money.new(0, "USD")
-
-# set Stripe API key
-Stripe.api_key = 'sk_test_o20UzauzBU7uIE2G7CoaMA3q00T3v4QoUU'
-
-# success & cancel URL's passed to Session. Currently under localhost on port 4567 - would be edited here if not running locally
-$success_url = 'http://localhost:4567/success?session_id={CHECKOUT_SESSION_ID}'
-$cancel_url = 'http://localhost:4567/cart'
+  # success & cancel URL's passed to Session. 
+  # currently under localhost on port 9292 - would be edited here if not running locally or on a different port
+  success_url = 'http://localhost:9292/success?session_id={CHECKOUT_SESSION_ID}'
+  cancel_url = 'http://localhost:9292/cart'
 
 
-get '/' do 
-	$products = create_album_hash(parse_products)
-	erb :index, :locals => {:products => $products}
-end
+  get '/' do 
+  	erb :index, :locals => {:products => @products}
+  end
 
-get '/cart' do
-	erb :cart, :locals => {:cart => $cart, :products => $products, :subtotal => $subtotal.cents / 100}
-end
+  get '/cart' do
+  	erb :cart, :locals => {:cart => @@cart, :products => @products, :subtotal => @@subtotal.cents / 100}
+  end
 
-get '/checkout' do
-	session = create_stripe_session
-  $session_id = session["id"]
-	erb :checkout, :locals => {:session_id => $session_id}
-end
+  get '/checkout' do
+  	session = create_stripe_session(@products, @@cart, success_url, cancel_url)
+    @@session_id = session["id"]
+  	erb :checkout, :locals => {:session_id => @@session_id}
+  end
 
-get '/success' do
-  charge_id = get_charge_id
-	erb :success, :locals => {:charge_id => charge_id, :subtotal => $subtotal.cents / 100}
-end
+  get '/success' do
+    @@cart.clear
+    saved_subtotal = @@subtotal
+    @@subtotal = Money.new(0, "USD")
+  	erb :success, :locals => {:charge_id => get_charge_id(@@session_id), :subtotal => saved_subtotal.cents / 100}
+  end
 
 
-post '/addToCart' do
-    $cart[params[:product_id]] += 1
-    $subtotal += Money.new($products[params[:product_id]].price.to_i * 100, "USD")
-    redirect '/'
-end
+  post '/addToCart' do
+      @@cart[params[:product_id]] += 1
+      @@subtotal += Money.new(@products[params[:product_id]].price.to_i * 100, "USD")
+      redirect '/'
+  end
 
-post '/removeFromCart' do
-    $cart[params[:product_id]] -= 1
-    $subtotal -= Money.new($products[params[:product_id]].price.to_i * 100, "USD")
-    $cart.delete(params[:product_id]) if $cart[params[:product_id]] < 1
-    redirect '/cart'
-end
+  post '/removeFromCart' do
+      @@cart[params[:product_id]] -= 1
+      @@subtotal -= Money.new(@products[params[:product_id]].price.to_i * 100, "USD")
+      @@cart.delete(params[:product_id]) if @@cart[params[:product_id]] < 1
+      redirect '/cart'
+  end
 
-post '/goToCart' do
-    redirect '/cart'
-end
+  post '/goToCart' do
+      redirect '/cart'
+  end
 
-post '/goToCheckout' do
-    redirect '/checkout'
-end
+  post '/goToCheckout' do
+      redirect '/checkout'
+  end
 
-helpers do
-  include Helpers
+  helpers do
+    include Helpers
+  end
+
 end
--- helpers.rb
+++ helpers.rb
@@ -6,9 +6,9 @@ module Helpers
   end
 
   # converts array of JSON product data into hash of products where product ID is the key and Album object is the value
-  def create_album_hash(products_array)
+  def create_album_hash(products)
     album_hash = {}
-    products_array.each do |product| 
+    products.each do |product| 
       album = Album.new(product["name"], product["metadata"]["price"], product["images"][0])
       album_hash[product["id"]] = album
     end
@@ -16,28 +16,28 @@ module Helpers
   end
 
   # creates session to pass id to redirectToCheckout for Checkout functionality
-  def create_stripe_session
+  def create_stripe_session(products, cart, success_url, cancel_url)
     items=[]
-    $cart.each do |product_id, quantity|
+    cart.each do |product_id, quantity|
         item = {}
-        item["amount"] = $products[product_id].price.to_i * 100
+        item["amount"] = products[product_id].price.to_i * 100
         item["currency"] = 'usd'
-        item["name"] = $products[product_id].name
+        item["name"] = products[product_id].name
         item["quantity"] = quantity
         items << (item)
       end
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: items,
-      success_url: $success_url,
-      cancel_url: $cancel_url
+      success_url: success_url,
+      cancel_url: cancel_url
   )
   end 
 
   #function to get 5 most recent charges and look to match to session on payment intent. NOTE: see README to see how I would use webhooks instead
-  def get_charge_id  
-    if $session_id != nil
-      session = Stripe::Checkout::Session.retrieve($session_id)
+  def get_charge_id(session_id) 
+    if session_id != nil
+      session = Stripe::Checkout::Session.retrieve(session_id)
       charge_list = Stripe::Charge.list({limit: 5})
       charge_list["data"].each do |charge|
         if charge["payment_intent"] == session["payment_intent"] && charge["status"] == "succeeded"
@@ -46,5 +46,5 @@ module Helpers
       end
     end
   end
-  
+
 end
